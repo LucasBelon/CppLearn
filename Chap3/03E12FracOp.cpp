d0fa87719ebe412db792e14a70849e72b49b4469
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-08-28 09:20:01 -0300

    last exercise...
    done?

--- Chap3/03E12FracOp.cpp
+++ Chap3/03E12FracOp.cpp
@@ -1,5 +1,5 @@
-/*Create a four-function calculator for fraction
- * . (See Exercise 9 in Chapter 2, and Exercise
+/* Create a four-function calculator for fraction.
+ * (See Exercise 9 in Chapter 2, and Exercise
  * 4 in this chapter.) Here are the formulas for
  * the four arithmetic operations applied to fractions:
  *
@@ -12,3 +12,61 @@
  * operator, and a second fraction. The program
  * should then display the result and ask whether
  * the user wants to continue.*/
+
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int frac1_dividendo,
+        frac1_divisor,
+        frac2_dividendo,
+        frac2_divisor,
+        result_dividendo,
+        result_divisor;
+    char operation;
+    char descarte;
+    int parar = 0;
+    while(parar !=1){
+        cout << "Escolha a operação: {+|-|*|/}: ";
+        cin >> operation;
+        cout << "Escreva a primeira fração [a/b]: ";
+        cin >> frac1_dividendo >> descarte >> frac1_divisor;
+        cout << "Escreva a segunda fração [c/d]: ";
+        cin >> frac2_dividendo >> descarte >> frac2_divisor;
+        switch (operation) {
+            case '+':
+                result_dividendo = 
+                    frac1_dividendo*frac2_divisor + 
+                    frac2_dividendo*frac1_divisor;
+                result_divisor =
+                    frac1_divisor * frac2_divisor;
+                break;
+            case '-':
+                result_dividendo = 
+                    frac1_dividendo*frac2_divisor - 
+                    frac2_dividendo*frac1_divisor;
+                result_divisor =
+                    frac1_divisor * frac2_divisor;
+                break;
+            case '*':
+                result_dividendo = 
+                    frac1_dividendo*frac2_dividendo;
+                result_divisor =
+                    frac1_divisor*frac2_divisor;
+                break;
+            case '/':
+                result_dividendo = 
+                    frac1_dividendo*frac2_divisor;
+                result_divisor =
+                    frac1_divisor*frac2_dividendo;
+                break;
+            default:
+                cout << "Operador válido não identificado"<< endl;
+        }
+        cout << "A fração resultado é: " << result_dividendo << "/" << result_divisor << endl;
+        cout << "Digite 1 para parar, 0 para contunuar: ";
+        cin >> parar;
+    }
+    return 0;
+}
+
