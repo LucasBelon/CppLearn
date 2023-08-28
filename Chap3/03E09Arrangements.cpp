85dd5ba7d24c5a50b45e3981398b76997b425dab
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-08-28 08:39:37 -0300

    exercise 09 done.

--- Chap3/03E09Arrangements.cpp
+++ Chap3/03E09Arrangements.cpp
@@ -1,12 +1,12 @@
-/* Suppose you give a dinner party for six guests
- * , but your table seats only four. In how many 
+/* Suppose you give a dinner party for six guests,
+ * but your table seats only four. In how many 
  * ways can four of the six guests arrange themselves 
  * at the table? Any of the six guests can sit 
  * in the first chair. Any of the remaining five
  * can sit in the second chair. Any of the remaining
  * four can sit in the third chair, and any of
- * the remaining three can sit in the fourth chai
- * . (The last two will have to stand.) So the
+ * the remaining three can sit in the fourth chairs
+ * (The last two will have to stand.) So the
  * number of possible arrangements of six guests
  * in four chairs is 6*5*4*3, which is 360. Write
  * a program that calculates the number of possible
@@ -15,3 +15,32 @@
  * be fewer guests than chairs.) Don’t let this
  * get too complicated. A simple for loop should
  * do it.*/
+
+# include <iostream>
+using namespace std;
+
+int main(void){
+    int seats;
+    int guests;
+    int combination = 1;
+    cout << "Digite o número de assentos: ";
+    cin >> seats;
+    cout << "Digite o número de convidados: " ;
+    cin >> guests;
+    if (guests < seats){
+        cout << 
+            "O número de convidados não pode ser\n\
+           \rmenor do que o número de cadeiras.\n\
+           \rTerminando programa"
+           << endl;
+        return 0;
+    }
+    for (int i = seats; i > 0; i--){
+        combination *= guests;
+        guests--;
+    }
+    cout << "The combinations are: " 
+         << combination << endl;
+
+    return 0;
+}
